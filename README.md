82f3ae88a508dfb2a5288bcb7ad3ff54eb0d48ee
piwi <me@e-piwi.fr>
2015-01-03 14:09:09 +0100

    new global license header & info

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This package defines some custom [Composer](http://getcomposer.org/) installers 
 Tools and Bundles.
 
 For more information, see <http://github.com/php-carteblanche/core/blob/master/doc/ComposerInstaller.md>.
+
+License Apache-2.0 <http://github.com/php-carteblanche/carteblanche/blob/master/LICENSE>.
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
--- src/CarteBlancheInstaller/CarteBlancheInstaller.php
+++ src/CarteBlancheInstaller/CarteBlancheInstaller.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * CarteBlanche - PHP framework package - Composer installer package
- * (c) Pierre Cassat and contributors
- * 
- * Sources <http://github.com/php-carteblanche/installer>
+ * This file is part of the CarteBlanche PHP framework.
  *
- * License Apache-2.0
+ * (c) Pierre Cassat <me@e-piwi.fr> and contributors
+ *
+ * License Apache-2.0 <http://github.com/php-carteblanche/carteblanche/blob/master/LICENSE>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
